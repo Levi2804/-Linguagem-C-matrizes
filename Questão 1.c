479a6b5f92351ee86d500e99a1fd3c2d5f731771
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 08:41:00 -0300

    Update and rename Questão 1.cpp to Questão 1.c

--- Questão 1.c
+++ Questão 1.c
@@ -6,7 +6,7 @@ int main(){
 	int m[3][3], l, c, i = 1;
 	for(l = 0; l < 3; l++){
 		for(c = 0; c < 3; c++){
-			printf("Digite o %iº valor da matriz: ", i++);
+			printf("Digite o %iÂº valor da matriz: ", i++);
 			scanf("%i", &m[l][c]);
 		}
 	}
